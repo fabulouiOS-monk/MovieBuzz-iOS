2c59030f269b2023bd3af42380cf2f21b1f9d9c0
Kailash Bora <kailash.bora@synchronoss.com>
2025-11-15 14:17:46 +0530

    Initial project setup

---
+++ MoviesBuzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoviesBuzz
+//
+//  Created by Kailash Bora on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoviesBuzz/MoviesBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesBuzzApp.swift
+//  MoviesBuzz
+//
+//  Created by Kailash Bora on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
